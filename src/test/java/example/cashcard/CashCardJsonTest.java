015a55f1eb062c884d30adafdabb004917893bc6
happyeun <optimizelse@gmail.com>
2024-03-07 16:38:10 +0900

    TDD을 위한 테스트 연습

---
+++ src/test/java/example/cashcard/CashCardJsonTest.java
@@ -0,0 +1,14 @@
+package example.cashcard;
+
+import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.assertThat;
+
+class CashCardJsonTest {
+
+   @Test
+   void myFirstTest() {
+      assertThat(1).isEqualTo(42); // TDD junit 사용해서.
+      //assertThat(42).isEqualTo(42);
+   }
+
+}
